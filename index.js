bcf9b1f10de5bd64d4afa072151cea75e5f6c3d1
MNdagu <ndagumark@gmail.com>
2024-07-03 19:29:00 +0300

    Final

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+function titleCased() {
+   return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ')
+      .map(word => {if(word === word.toLowerCase()){
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+
+      }else{
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+       
+    })
+    
+      .join(' ');
+  });
 }
+
+console.log(titleCased());
